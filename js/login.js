a620e1892338f26d1beeae0cf04bb1a748674073
Facundo Rodriguez Perdomo <facundorodriguez7714@gmail.com>
2021-09-16 20:36:36 -0300

    entrega3

--- js/login.js
+++ js/login.js
@@ -100,11 +100,3 @@ function onSignIn(googleUser) {
 /** FONDO ANIMADO PENDIENTE */
 
 
-/** ESTO VINO CON LA PROPUESTA LO DEJO APARTE **/
-
-/**Función que se ejecuta una vez que se haya lanzado el evento de
-que el documento se encuentra cargado, es decir, se encuentran todos los
-elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-});**/
--- js/product-info.js
+++ js/product-info.js
@@ -1,14 +1,13 @@
 //ENTREGA 3 TABLA CON DESCRIPCION DEL PRODUCTO
 document.addEventListener("DOMContentLoaded", function (e) {
-
-  let url = 'https://japdevdep.github.io/ecommerce-api/product/5678.json'
+  let url = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
 
   fetch(url)
-    .then(response => response.json())
-    .then(dataProduct => mostrarData(dataProduct))
-    .catch(error => console.log(error))
+    .then((response) => response.json())
+    .then((dataProduct) => mostrarData(dataProduct))
+    .catch((error) => console.log(error));
   const mostrarData = (dataProduct) => {
-    let body = ''
+    let body = "";
     {
       body += ` <table class="table table-bordered table-striped">
             <thead>
@@ -20,30 +19,48 @@ document.addEventListener("DOMContentLoaded", function (e) {
                     <td>${dataProduct.soldCount}</td>
                     <td>${dataProduct.category}</td>
                     <td>${dataProduct.relatedProducts}</td>
-                    
                 </tr>
             </thead>
-            <tbody id="dataProductID">
-            </tbody>
-        
-        </table>  `
+          <tbody id="dataProductID"></tbody>
+          </table>  `;
     }
-    document.getElementById('dataProductID').innerHTML = body
-  }
+    document.getElementById("dataProductID").innerHTML = body;
+  };
 });
 
 //ENTREGA 3 OPINIONES DE USUARIOS JSON
 document.addEventListener("DOMContentLoaded", function (e) {
-
-  let url = 'https://japdevdep.github.io/ecommerce-api/product/5678-comments.json'
+  let url =
+    "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
 
   fetch(url)
-    .then(response => response.json())
-    .then(dataProductOpinion => mostrarData(dataProductOpinion))
-    .catch(error => console.log(error))
+    .then((response) => response.json())
+    .then((dataProductOpinion) => mostrarData(dataProductOpinion))
+    .catch((error) => console.log(error));
   const mostrarData = (dataProductOpinion) => {
-    let body1 = ''
+    let estrellas = ` <div class="col-12" style="text-align: center;">
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="1estrella"></span>
+  
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="2estrella"></span>
+  
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="3estrella"></span>
+  
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="4estrella"></span>
+  
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="5estrella"></span>
+    
+  
+  </div> `;
+
+    let body1 = "";
     for (let i = 0; i < dataProductOpinion.length; i++) {
+      // dataProductOpinion[i].score = calificar(item);
+
       body1 += ` <div class="container-comments" id="CajaDeNuevoComentario">
             <div class="comments">
               <div class="info-comments">
@@ -52,51 +69,49 @@ document.addEventListener("DOMContentLoaded", function (e) {
                   <h5>${dataProductOpinion[i].dateTime}</h5>
                 </div>
                 <p>${dataProductOpinion[i].description}</p>
-        
-                <div>
-                  <p>${dataProductOpinion[i].score}</p>
-                  <form>
-  <p class="clasificacion">
-    <input id="radio1" type="radio" name="estrellas" value="5">
-    <label for="radio1">★</label>
-    <input id="radio2" type="radio" name="estrellas" value="4">
-    <label for="radio2">★</label>
-    <input id="radio3" type="radio" name="estrellas" value="3">
-    <label for="radio3">★</label>
-    <input id="radio4" type="radio" name="estrellas" value="2">
-    <label for="radio4">★</label>
-    <input id="radio5" type="radio" name="estrellas" value="1">
-    <label for="radio5">★</label>
-  </p>
-</form>
-                </div>
-        
-                <div class="footer">
+              <div>
+                ${estrellas}
+              </div>
+               <div class="footer">
                   <label class="icon-bubble"></label>
                 </div>
-        
-              </div>
+        </div>
             </div>
-          </div> `
+          </div> `;
     }
-    document.getElementById('CajaDeNuevoComentario').innerHTML += body1
-  }
-
+    document.getElementById("CajaDeNuevoComentario").innerHTML += body1;
+  };
 });
 
 //ENTREGA 3 ... NUEVOS COMENTARIOS
 document.addEventListener("DOMContentLoaded", function (e) {
-
-  var boton = document.getElementById('button');
+  var boton = document.getElementById("button");
 
   boton.addEventListener("click", function () {
-    var comentaAqui = document.getElementById('comenta_aqui').value;
-    var suNombre = document.getElementById('NombreUsuarioComentario').value;
-    document.getElementById('comenta_aqui').value = ""; //limpia luego de usarlos
-    document.getElementById('NombreUsuarioComentario').value = "";
+    var comentaAqui = document.getElementById("comenta_aqui").value;
+    var suNombre = document.getElementById("NombreUsuarioComentario").value;
+    document.getElementById("comenta_aqui").value = ""; //limpia luego de usarlos
+    document.getElementById("NombreUsuarioComentario").value = "";
+    let estrellas = ` <div class="col-12" style="text-align: center;">
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="1estrella"></span>
+  
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="2estrella"></span>
+  
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="3estrella"></span>
+  
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="4estrella"></span>
+  
+    <span class="fa fa-star" onclick="calificar(this)"
+    style="cursor: pointer;" id="5estrella"></span>
+    
+  
+  </div> `;
 
-
-    let body1 = ''
+    let body1 = "";
 
     body1 += ` <div class="container-comments" id="CajaDeNuevoComentario">
     <div class="comments">
@@ -106,35 +121,34 @@ document.addEventListener("DOMContentLoaded", function (e) {
           <h5>Fecha</h5>
         </div>
         <p>${comentaAqui}</p>
-
-        <div>
-        <form>
-  <p class="clasificacion">
-    <input id="radio1" type="radio" name="estrellas" value="5">
-    <label for="radio1">★</label>
-    <input id="radio2" type="radio" name="estrellas" value="4">
-    <label for="radio2">★</label>
-    <input id="radio3" type="radio" name="estrellas" value="3">
-    <label for="radio3">★</label>
-    <input id="radio4" type="radio" name="estrellas" value="2">
-    <label for="radio4">★</label>
-    <input id="radio5" type="radio" name="estrellas" value="1">
-    <label for="radio5">★</label>
-  </p>
-</form>
+       <div>
+        ${estrellas}
         </div>
-
         <div class="footer">
           <label class="icon-bubble"></label>
         </div>
 
       </div>
     </div>
-  </div> `
-    document.getElementById('CajaDeNuevoComentario').innerHTML += body1
+  </div> `;
+    document.getElementById("CajaDeNuevoComentario").innerHTML += body1;
   });
 });
 
+// FUNCION PARA MARCAR LAS ESTRELLAS
+var contador;
+function calificar(item) {
+  console.log(item);
+  contador = item.id[0]; //primer caracter
+  let nombre = item.id.substring(1); // captura todos menos el primero
 
+  for (let i = 0; i < 5; i++) {
+    if (i < contador) {
+      document.getElementById(i + 1 + nombre).style.color = "orange";
+    } else {
+      document.getElementById(i + 1 + nombre).style.color = "black";
+    }
+  }
+}
 
-
+calificar(item);
